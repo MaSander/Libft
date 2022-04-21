cf55568f1fa9a2b82848528295745f338eed1307
MaSander <ma.sander11@gmail.com>
2022-04-21 04:02:36 +0200

    finish the fuking strtrim

--- v00/ft_strtrim.c
+++ v00/ft_strtrim.c
@@ -17,50 +17,33 @@
 size_t	find_trimmed_amount(char const *s1, char const *set)
 {
 	size_t	len;
-	int		i;
-	char	*r;
 
 	len = 1;
-	i = 0;
-	while(s1[i])
+	while (*s1)
 	{
-		r = ft_strrchr(set, *s1);
-		if(r == 0)
+		if (ft_strrchr(set, *s1) == 0)
 			len++;
-		i++;
+		s1++;
 	}
-
 	return (len);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
-	int		count;
-	int		isequal;
 	char	*trimmed;
 
 	i = 0;
-	count = 0;
-	isequal = 0;
 	if (set == NULL)
-		return NULL;
-	trimmed = malloc(find_trimmed_amount(s1, set));
+		return (NULL);
+	trimmed = ft_calloc(find_trimmed_amount(s1, set), 1);
 	while (*s1)
 	{
-		while (set[i])
+		if (ft_strrchr(set, *s1) == 0)
 		{
-			if(*s1 == set[i])
-				isequal = 1;
+			trimmed[i] = *s1;
 			i++;
 		}
-		if (isequal == 0)
-		{
-			trimmed[count] = *s1;
-			count++;
-		}
-		isequal = 0;
-		i = 0;
 		s1++;
 	}
 	return (trimmed);
