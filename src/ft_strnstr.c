bb943600a840b52551bcd7e624ce9ff7f94b2902
Marcelo Sander <42551183+MaSander@users.noreply.github.com>
2022-10-05 20:28:49 -0300

    include ft_strncmp in file

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -11,6 +11,22 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
+
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (n && (str1[i] || str2[i]))
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+		n--;
+	}
+	return (0);
+}
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
